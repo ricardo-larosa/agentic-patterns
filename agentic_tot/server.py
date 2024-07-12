b9a63860a975adc47486df5a3c4f23032825d393
ricardo-larosa <ricardo@larosa.dev>
2024-07-12 23:41:18 +0200

    Use Redis

--- agentic_tot/server.py
+++ agentic_tot/server.py
@@ -13,12 +13,11 @@ from llama_agents import (
 from llama_index.core.agent import FunctionCallingAgentWorker
 from llama_index.core.tools import FunctionTool
 from llama_index.llms.groq import Groq
-from llama_agents.message_queues.rabbitmq import RabbitMQMessageQueue
-# from llama_agents.message_queues.redis import RedisMessageQueue
+from llama_index.llms.openai import OpenAI
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from llama_index.core.llms import ChatMessage, MessageRole
+from llama_agents.message_queues.redis import RedisMessageQueue
 
-# from toolset.code import parse_code_block
 # create an agent
 def get_the_secret_fact() -> str:
     """Returns the secret fact."""
@@ -27,18 +26,27 @@ def get_the_secret_fact() -> str:
 
 tool = FunctionTool.from_defaults(fn=get_the_secret_fact)
 
-worker1 = FunctionCallingAgentWorker.from_tools([tool], llm=Groq(model="llama3-8b-8192"))
-worker2 = FunctionCallingAgentWorker.from_tools([], llm=Groq(model="llama3-8b-8192"))
+worker1 = FunctionCallingAgentWorker.from_tools(
+    tools=[tool], 
+    llm=Groq(model="llama3-8b-8192"))
+worker2 = FunctionCallingAgentWorker.from_tools(
+    system_prompt="You are a helpful assistant that tells dumb fact.",
+    llm=Groq(model="llama3-8b-8192"),
+    verbose=True)
+
 agent1 = worker1.as_agent()
 agent2 = worker2.as_agent()
 
 # create our multi-agent framework components
-# message_queue = RedisMessageQueue()
-message_queue = RabbitMQMessageQueue()
+message_queue = RedisMessageQueue()
+# message_queue = RabbitMQMessageQueue()
 
 control_plane = ControlPlaneServer(
     message_queue=message_queue,
-    orchestrator=AgentOrchestrator(llm=Groq(model="llama3-70b-8192")),
+    orchestrator=AgentOrchestrator(
+        llm=Groq(model="llama3-70b-8192")
+        ),
+    port=8000,
 )
 agent_server_1 = AgentService(
     agent=agent1,
@@ -64,11 +72,9 @@ human_service = HumanService(
     port=8004,
 )
 
-
 # additional human consumer
 def handle_result(message: QueueMessage) -> None:
     print("Got result:", message.data)
-
 
 human_consumer = CallableMessageConsumer(handler=handle_result, message_type="human")
 
